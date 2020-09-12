442fb04b6fe0e1ac4a5d5116ee7b424ed1c2c701
Joboa <joboa2015@gmail.com>
2020-09-12 20:00:48 +0000

    components created: Navbar, Footer, Blog and About

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to="#">studyflow</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home <span className="sr-only"></span></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/insight">Insight</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                       
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/blog">Blog</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Fluid
+                  </Link>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <Link className="dropdown-item" to="#">Action</Link>
+                                <div className="dropdown-divider"></div>
+                                <Link className="dropdown-item" to="#">Another action</Link>
+                                <div className="dropdown-divider"></div>
+                                <Link className="dropdown-item" to="#">Something else here</Link>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/sigup">Sign Up</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                    </ul>
+            </div>
+          </nav>
+        )
+    }
+}
