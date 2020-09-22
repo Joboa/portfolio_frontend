83797446d3b0670173be38131b61f603b6feb132
Joboa <joboa2015@gmail.com>
2020-09-22 16:01:20 +0000

    Added two images in the images folder

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,8 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa temporibus distinctio molestias esse, in natus eveniet laudantium dolorum ipsa unde sunt rerum corrupti atque! Ex maxime vitae ipsum expedita saepe.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa temporibus distinctio molestias esse, 
+                in natus eveniet laudantium dolorum ipsa unde sunt rerum corrupti atque! Ex maxime vitae ipsum expedita saepe.
             </div>
         )
     }
